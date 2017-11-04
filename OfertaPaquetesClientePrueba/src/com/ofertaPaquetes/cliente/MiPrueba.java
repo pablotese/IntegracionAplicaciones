17e3adb960b524e783c8ec1b6a1e6fd853346606
vguccione <vguccione@gmail.com>
2017-11-04 00:24:30 -0300

    Agrego manejo de mensajeria. La imagen del paquete es un string.

--- OfertaPaquetesClientePrueba/src/com/ofertaPaquetes/cliente/MiPrueba.java
+++ OfertaPaquetesClientePrueba/src/com/ofertaPaquetes/cliente/MiPrueba.java
@@ -22,36 +22,30 @@ public class MiPrueba {
 		
 		/*Test Agencia*/
 		PaisDTO pais= new PaisDTO(1,"Argentina");
-		AgenciaDTO ag = new AgenciaDTO("Prueba",false, "calle",123,"1","C","capital Federal",pais, new Date());
-		
+		AgenciaDTO ag = new AgenciaDTO("Prueba Agencia",false, "calle",123,"1","C","capital Federal",pais, new Date());
+		ag.setIdAgenciaBO(23);
 		try {
 			bd.nuevaAgencia(ag);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		List<AgenciaDTO> agencias = bd.listarAgenciasPorEstado(false);
-		if(agencias!=null){
-			for(AgenciaDTO dto:agencias){
-				System.out.println(dto.toString());
-			}
-		}
+		System.out.println("pepeurlo");
 		
 		/*Test Paquetes*/
-		PaqueteDTO paquete = new PaqueteDTO("Paquete 1",new Date(),new Date(),"Descripcion paquete 1",220D,"",10,12,false,true);
+		//PaqueteDTO paquete = new PaqueteDTO("Paquete 1",new Date(),new Date(),"Descripcion paquete 1",220D,"",10,12,false,true);
 		
 		List<AgenciaDTO> agenciasDto =  bd.listarAgencias();
-		
 		AgenciaDTO agencia = new AgenciaDTO(agenciasDto.get(0).getIdAgencia());
-		paquete.setAgencia(agencia);
-		DestinoDTO destino = new DestinoDTO(1);
-		paquete.setDestino(destino);
+		DestinoDTO destino = new DestinoDTO(1, "Miami");
 		
-		List<ImagenDTO> imagenes = new ArrayList<ImagenDTO>();
-		paquete.setImagenes(imagenes);
-		List<TipoServicioDTO> servicios = new ArrayList<TipoServicioDTO>();
-		paquete.setServicios(servicios);
+		//Los tipos de servicio vienen de un WS de BO
+		List<TipoServicioDTO> servicios = new ArrayList<TipoServicioDTO>(); 
+		servicios.add(new TipoServicioDTO("Wifi", "Internet"));
+		servicios.add(new TipoServicioDTO("Pileta", "Re pileta")); 
+		
+		PaqueteDTO paquete = new PaqueteDTO("Paquete 1 Miami", new Date(2017, 11,1), new Date(2017, 12,1), 
+				"Paquete 1 viaje a Miami", (double) 12000, "No cancelable", 40, 2, true, true, "Miami.jpg", agencia, destino, servicios);
 		
 		try{
 			bd.nuevoPaquete(paquete);
@@ -64,6 +58,7 @@ public class MiPrueba {
 		PaqueteDTO paqdto = bd.obtenerPaquete(paquetesDto.get(0).getIdPaquete());
 		
 		System.out.println(paqdto.toString());
-	}
+		System.out.println("Hola Mndo!!!");	
+		}
 
 }
