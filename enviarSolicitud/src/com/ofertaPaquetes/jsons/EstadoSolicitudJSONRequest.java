54d7c03bcdce714bc85c28b45660ee232575eb20
dcorvetto <danacvtt@gmail.com>
2017-11-06 16:13:24 -0300

    Dummys

--- enviarSolicitud/src/com/ofertaPaquetes/jsons/EstadoSolicitudJSONRequest.java
+++ enviarSolicitud/src/com/ofertaPaquetes/jsons/EstadoSolicitudJSONRequest.java
@@ -1,47 +1,37 @@
 package com.ofertaPaquetes.jsons;
 
-public class EstadoSolicitudJSON {
+public class EstadoSolicitudJSONRequest {
 	
-	int id;
+	
 	String tipo;
 	String detalle;
-	String estado;
-	public EstadoSolicitudJSON() {
+	
+	public EstadoSolicitudJSONRequest() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public EstadoSolicitudJSON(int id, String tipo, String detalle, String estado) {
+
+	public EstadoSolicitudJSONRequest(String tipo, String detalle) {
 		super();
-		this.id = id;
 		this.tipo = tipo;
 		this.detalle = detalle;
-		this.estado = estado;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public String getDetalle() {
 		return detalle;
 	}
+
 	public void setDetalle(String detalle) {
 		this.detalle = detalle;
 	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	
 	
 	
 	
